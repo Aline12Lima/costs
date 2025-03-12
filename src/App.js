de71c1fb6f51d75a2533fcbe29449f0d431062bc
Aline Lima <aline.souza21@outlook.com.br>
2025-03-12 16:36:37 -0300

    Add Componentes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import {  BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom'
+import Home from './components/Pages/Home.js'
+import  Company from './components/Pages/Company.js'
+import Contact from './components/Pages/Contact.js'
+import NewProject from './components/Pages/NewProject.js'
+import Container from './components/Layout/Container.js'
+
 function App() {
   return (
-    <div className="App">
-      <p> Costs</p>
-    </div>
+
+    <Router>
+      
+        <Container>
+        <Link to='/'> Home </Link>
+        <Link to='/contact'> contact </Link>
+        <Link to='/company'> Company </Link>
+        <Link to='/newproject'> New Project</Link>
+        </Container>
+         <Routes>  
+      
+           <Route exact path='/' element={<Home />} />
+           <Route exact path='/company' element={<Company />} />
+
+           <Route exact path='/contact' element={<Contact />} />
+
+          <Route exact path='/newproject' element={<NewProject />} />
+
+        </Routes>
+       
+      <footer>Footer</footer>
+    </Router>
+      
   );
 }
 
