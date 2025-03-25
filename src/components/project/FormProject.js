b18fd7b8e743f2e28a5005aaea1a3ed099bb1b2d
Aline Lima <aline.souza21@outlook.com.br>
2025-03-25 14:59:34 -0300

    Add Bancojs, useEfecct no form

--- src/components/project/FormProject.js
+++ src/components/project/FormProject.js
@@ -1,9 +1,24 @@
+import { useEffect, useState } from 'react'
 import style from './FormProject.module.css'
 import Input from '../form/Input'
 import Select from '../select/Select'
 import Button from '../button/Button'
 
 function FormProject(btnText){
+    const[categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories",{
+            method: "GET",
+            headers:{
+                'Content-Type': 'application/json',
+            },
+        })
+        .then((resp) => resp.json())
+        .then((data) => {setCategories(data)})
+        .catch((err) => console.log(err))
+    },[])
+
     return(
     <form className={style.form}>
         <Input 
@@ -20,6 +35,7 @@ function FormProject(btnText){
         <Select
          name='category_id' 
          text="Selecione a categoria"
+         options={categories}
 
         />
         <Button text={btnText}/>
